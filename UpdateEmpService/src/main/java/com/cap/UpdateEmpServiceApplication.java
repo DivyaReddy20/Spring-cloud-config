6c7871b742454bf167583fa3d5d7fa0861f9bce8
DIVYA <DIVYA@LAPTOP-JTHODNGL>
2020-04-20 17:39:24 +0530

    commit

--- UpdateEmpService/src/main/java/com/cap/UpdateEmpServiceApplication.java
+++ UpdateEmpService/src/main/java/com/cap/UpdateEmpServiceApplication.java
@@ -3,6 +3,8 @@ package com.cap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +13,9 @@ public class UpdateEmpServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UpdateEmpServiceApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+		
+	}
 }
